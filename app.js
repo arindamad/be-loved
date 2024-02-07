eb22dd08ab143e54e830ea93e3982b3647cfa397
Arindam Sarkar <arindamx01@gmail.com>
2024-02-07 18:54:40 +0530

    Update in sound

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
+    let clickCound = 0;
     $('.title').click(function(){
       $('.container').addClass('open');
+      if(clickCound===0){
+        const Audio = new Audio("./sound.mpeg").play(); 
+        clickCound++;
+      }
+           
     });
     
     
